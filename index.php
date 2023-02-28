ee394e5aeefc0dee399fa5951a9b59c13e012e0a
alighadi <117599554+Niushaaaa@users.noreply.github.com>
2023-02-28 22:40:55 +0330

    اضافه كردن فيلد و متغير هاي مورد نظر مثل كد ملي رشته سن و كد ملي

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
-<?php session_start();?>
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>session</title>
-</head>
-</html>
+<?php session_start(); ?>
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>session</title>
+    </head>
+    </html>
 <?php
-if (isset($_POST{"submit"})){
-    $name=$_POST['name'];
-    $last_name=$_POST['last name'];
-    $_SESSION['name']=$name;
-    $_SESSION['last name']=$last_name;
+if (isset($_POST{"submit"})) {
+    $name = $_POST['name'];
+    $_SESSION['name'] = $name;
+    $last_name = $_POST['last name'];
+    $_SESSION['last name'] = $last_name;
+    $id_serial_number = $_POST['id_serial_number'];
+    $_SESSION['id_serial_number'] = $id_serial_number;
+    $subject = $_POST['subject '];
+    $_SESSION['subject '] = $subject;
+    $age = $_POST['age'];
+    if
 }
